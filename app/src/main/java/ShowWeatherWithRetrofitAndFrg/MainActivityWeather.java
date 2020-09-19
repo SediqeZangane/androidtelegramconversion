39e58380a1cdccd8412a3ad8f9ef4d318af7044a
ASUS <se.zanganeh@yahoo.com>
2020-09-20 01:37:27 +0430

    weather_UI

---
+++ app/src/main/java/ShowWeatherWithRetrofitAndFrg/MainActivityWeather.java
@@ -0,0 +1,16 @@
+package ShowWeatherWithRetrofitAndFrg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidtelgramconversion.R;
+
+public class MainActivityWeather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_weather);
+    }
+}
