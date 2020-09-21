1cbc389d411ae95fc2cfc1064d92058a0e781858
ASUS <se.zanganeh@yahoo.com>
2020-09-21 16:07:19 +0330

    UI-Page Two

--- app/src/main/java/ShowWeatherWithRetrofitAndFrg/MainActivityWeather.java
+++ app/src/main/java/ShowWeatherWithRetrofitAndFrg/MainActivityWeather.java
@@ -11,6 +11,6 @@ public class MainActivityWeather extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_weather);
+        setContentView(R.layout.fragment_show_weather);
     }
 }
